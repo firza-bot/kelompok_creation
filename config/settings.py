a56bf8191b4d1234dd49dd507abaf228c67fbccf
Jmerio-dot <jmerpmer@gmail.com>
2026-06-25 00:07:18 +0700

    feat: allow UIUX_DB_PATH to be overridden by env var

--- config/settings.py
+++ config/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
     },
     'uiux_db': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.environ.get('UIUX_DB_PATH', BASE_DIR / 'db.sqlite3'),
     }
 }
 
